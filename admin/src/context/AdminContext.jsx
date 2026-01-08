09b1aa8ea750e1bb2c4a8e24cd51907a150e7855
Altaf Raja <altafraja01076@gmail.com>
2026-01-09 00:10:01 +0530

    feat: Add admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider = ({children}) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')? localStorage.getItem('aToken') : '')
     const [doctors,setDoctors] = useState([])
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -87,12 +88,28 @@ const AdminContextProvider = ({children}) => {
 
     }
 
+
+    const getDashData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/admin/dashboard', {headers:{aToken}})
+            if(data.success){
+                setDashData(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
     const value = {
         aToken, setAToken,
         backendUrl,doctors,
         getAllDoctors, changeAvailability,
         appointments, getAllAppointments,setAppointments,
         cancelAppointment,
+        dashData, getDashData
 
     }
 
